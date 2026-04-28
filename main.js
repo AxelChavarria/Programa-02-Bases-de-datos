675c1394e8db152c0011f3e0748c8facb656b3c6
Cassie0424 <kassandrarodri04@gmail.com>
2026-04-28 17:31:10 -0600

    Función iniciar sesión
    lista la parte para iniciar sesión falta conectar con la base

---
+++ main.js
@@ -0,0 +1,10 @@
+//log in
+
+const formLogin = document.getElementById("form-login");
+if (formLogin) {
+    let user = document.getElementById("user").value.trim()
+    let password = document.getElementById("contra").value.trim()
+
+    //aqui llamamos a la función para iniciar sesión
+
+}
