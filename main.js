cc820c4c8bc309df44c77c00fc3f3799763d8458
Cassie0424 <kassandrarodri04@gmail.com>
2026-04-28 21:38:15 -0600

    Insertar empleado
    lista función de insertar empleado

--- main.js
+++ main.js
@@ -13,8 +13,12 @@ if (formLogin) {
 
 const tablaEmpleados = document.getElementById("empleados");
 if (tablaEmpleados) {
+
+    //obtenemos la información
     //let informacion = cargarDatos() //traer datos
 
+
+    //desplegamos la información
     //informacion.forEach(emp => { //${emp.} //pasar por las listas
         tablaEmpleados.innerHTML += `
             <tr data-id="1">
@@ -22,9 +26,17 @@ if (tablaEmpleados) {
                 <td>ref</td>
                 <td>ref</td>
                 <td>ref</td>
+                <td class="acciones">
+                    <button onclick="window.location.href='actualizar.html'" class="form-btn">Actualizar</button>
+                    <button onclick="window.location.href='borrar.html'" class="form-btn">Borrar</button>
+                    <button onclick="window.location.href='consultar.html'" class="form-btn">Consultar</button>
+                    <button onclick="window.location.href='listar_movimientos.html'" class="form-btn">Listar movimientos</button>
+                    <button onclick="window.location.href='agregar_movimiento.html'" class="form-btn">Agregar movimientos</button>
+                </td>
             </tr>`;
     //});
 
+    //boton para filtrar con validaciones 
     const boton = document.getElementById("buscar");
 
     boton.addEventListener("click", function(event) {
@@ -33,22 +45,30 @@ if (tablaEmpleados) {
         const valor = document.getElementById("busqueda").value.trim()
         
         if (/^\d+$/.test(valor)){
-            console.log("Si")
+            console.log("numeros")
+        }
+        
+        if (/^[a-zA-Z]+$/.test(texto)) {
+            console.log("Solo letras");
         }
         
     });
 
-
-    tablaEmpleados.addEventListener("click", function(e) {
-    const fila = e.target.closest("tr");
-    if (!fila) return;
-
-    const id = fila.dataset.id;
-
-    sessionStorage.setItem("idSeleccionado", id);
-
-    window.location.href = "botones.html";
-});
 }
 
 //const id = sessionStorage.getItem("idSeleccionado");  //para llamar el id
+//insertar empleado
+
+const formInsertar = document.getElementById("form-insertar");
+if (formInsertar) {
+
+    formInsertar.addEventListener("submit", function(e) {
+        event.preventDefault();
+        
+        let identificacion = document.getElementById("identidad").value.trim()
+        let nombre = document.getElementById("nombre").value.trim()
+        let puesto = document.getElementById("puesto").value.trim()
+
+        //aqui va la funcion para llamar a la base
+    });
+}
