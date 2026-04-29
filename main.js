8ce3b9fffa4c0be52c1732afdc4e08fa3850271a
Cassie0424 <kassandrarodri04@gmail.com>
2026-04-28 19:48:10 -0600

    listar empleados
    lista la tabla de listar empleados, al seleccionar la linea que se desea consultar lleva al apartado de botones

--- main.js
+++ main.js
@@ -8,3 +8,47 @@ if (formLogin) {
     //aqui llamamos a la función para iniciar sesión
 
 }
+
+//lista de empleados 
+
+const tablaEmpleados = document.getElementById("empleados");
+if (tablaEmpleados) {
+    //let informacion = cargarDatos() //traer datos
+
+    //informacion.forEach(emp => { //${emp.} //pasar por las listas
+        tablaEmpleados.innerHTML += `
+            <tr data-id="1">
+                <td>ref</td>
+                <td>ref</td>
+                <td>ref</td>
+                <td>ref</td>
+            </tr>`;
+    //});
+
+    const boton = document.getElementById("buscar");
+
+    boton.addEventListener("click", function(event) {
+        event.preventDefault();     
+
+        const valor = document.getElementById("busqueda").value.trim()
+        
+        if (/^\d+$/.test(valor)){
+            console.log("Si")
+        }
+        
+    });
+
+
+    tablaEmpleados.addEventListener("click", function(e) {
+    const fila = e.target.closest("tr");
+    if (!fila) return;
+
+    const id = fila.dataset.id;
+
+    sessionStorage.setItem("idSeleccionado", id);
+
+    window.location.href = "botones.html";
+});
+}
+
+//const id = sessionStorage.getItem("idSeleccionado");  //para llamar el id
